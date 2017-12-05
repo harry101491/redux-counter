c86252ae958d160a94824267d2dc02233c16e093
Harshit Pareek <hp1014@nyu.edu>
2017-12-05 18:19:39 -0500

    feat(actions)

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,29 @@
-import { INCREMENT, DECREMENT } from "./actionTypes";
+// action creators in the todo list
+import { ADD_TODO, TOGGLE_TODO, SET_VISIBILITY_FILTER, FILTERS } from "./actionTypes";
 
-// defining the action creators for the counter-redux app
-export const incrementAction = () => {
-    return {
-        type: INCREMENT
-    };
+export const addTodo = (text) => {
+    return { type: ADD_TODO, text: text};
 }
 
-export const decrementAction = () => {
-    return {
-        type: DECREMENT
-    };
+export const toggleTodo = (index) => {
+    return { type: TOGGLE_TODO, index: index };
 }
+
+export const setVisibilityFilter = (filter) => {
+    return { type: SET_VISIBILITY_FILTER, filter: filter };
+}
+
+// import { INCREMENT, DECREMENT } from "./actionTypes";
+
+// // defining the action creators for the counter-redux app
+// export const incrementAction = () => {
+//     return {
+//         type: INCREMENT
+//     };
+// }
+
+// export const decrementAction = () => {
+//     return {
+//         type: DECREMENT
+//     };
+// }
