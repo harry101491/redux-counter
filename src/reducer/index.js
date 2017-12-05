95ed4d4abe55bd9d5f9cca0ff84515b42c33c839
Harshit Pareek <hp1014@nyu.edu>
2017-12-05 18:42:32 -0500

    feat(actions)

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,12 +1,28 @@
-import { INCREMENT, DECREMENT } from "../actions/actionTypes";
+// reducers for the todo app
+import { FILTERS } from "../actions/actionTypes";
 
-export default (state = 0, action) => {
+const initialState = { 
+    todos: [],
+    visibilityFilter: FILTERS.SHOW_ALL
+}
+
+export const todoReducer = (state = initialState, action) => {
     switch(action.type) {
-        case "INCREMENT":
-           return state+1;
-        case "DECREMENT":
-            return state-1;
-        default:
-            return state;
+        
     }
 }
+
+
+
+// import { INCREMENT, DECREMENT } from "../actions/actionTypes";
+
+// export default (state = 0, action) => {
+//     switch(action.type) {
+//         case "INCREMENT":
+//            return state+1;
+//         case "DECREMENT":
+//             return state-1;
+//         default:
+//             return state;
+//     }
+// }
