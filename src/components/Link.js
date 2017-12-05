8ca8ae624576a65f17d0a59ce462ad3254d42b9d
Harshit Pareek <hp1014@nyu.edu>
2017-12-04 23:45:08 -0500

    feat(link)

---
+++ src/components/Link.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Link extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return ( 
+            (this.props.active) ? 
+                (<span>{children}</span>) : 
+                (<a 
+                    href="" 
+                    onClick={ e => {
+                        e.preventDefault();
+                        this.props.onClick();
+                }} >
+                    { this.props.children }
+                </a>)
+            );
+    }
+}
+
+Link.propTypes = {
+    active: PropTypes.bool.isRequired,
+    children: PropTypes.node.isRequired,
+    onClick: PropTypes.func.isRequired
+}
+
+export default Link;
