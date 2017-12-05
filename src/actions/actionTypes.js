6b63d137d221d6f0e08c20a455107b8054535b32
Harshit Pareek <hp1014@nyu.edu>
2017-12-05 18:09:47 -0500

    feat(actions)

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,2 +1,7 @@
-export const INCREMENT = "INCREMENT";
-export const DECREMENT = "DECREMENT";
+// export const INCREMENT = "INCREMENT";
+// export const DECREMENT = "DECREMENT";
+
+// all action types for the todo list app
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
